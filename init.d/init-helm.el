0abcbae4e30bebf8d08b362e6fdcda3c58b7a5c0
Sumihiko Natsu <hottestseason@gmail.com>
2015-05-08 16:45:54 +0900

    Use ag to search project

--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -27,7 +27,7 @@
     (global-set-key (kbd "M-S") 'helm-projectile-ag)))
 
 (global-set-key (kbd "M-s") 'helm-occur)
-(global-set-key (kbd "M-S") 'helm-projectile-ack)
+(global-set-key (kbd "M-S") 'helm-projectile-ag)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-z") 'helm-resume)
